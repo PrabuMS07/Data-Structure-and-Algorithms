a53a9c16a53f5063d2335b78b5f3f5b3e10287b5
Prabu000 <msprabu12345@gmail.com>
2025-04-29 04:30:58 +0530

    167. Two Sum II - Input Array Is Sorted

--- Array & String/1800. Maximum Ascending Subarray Sum.py
+++ Array & String/1800. Maximum Ascending Subarray Sum.py
@@ -1,3 +1,19 @@
+"""
+
+Given an array of positive integers nums, return the maximum possible sum of an strictly increasing subarray in nums.
+
+A subarray is defined as a contiguous sequence of numbers in an array.
+
+ 
+
+Example 1:
+
+Input: nums = [10,20,30,5,10,50]
+Output: 65
+Explanation: [5,10,50] is the ascending subarray with the maximum sum of 65.
+
+"""
+
 class Solution(object):
     def maxAscendingSum(self, nums):
 
