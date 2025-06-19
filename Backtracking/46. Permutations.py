075f30c0756b1cbae2ce3fb5b879d14f761e8431
prabums07 <msprabu12345@gmail.com>
2025-06-19 07:24:26 +0530

    46. Permutations with 1. Recursion and 2. Iteration

--- Backtracking/46. Permutations.py
+++ Backtracking/46. Permutations.py
@@ -8,6 +8,9 @@ Input: nums = [1,2,3]
 Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
 """
 
+# 1. Recursion 
+
+
 class Solution(object):
     def permute(self, nums):
 
@@ -24,6 +27,25 @@ class Solution(object):
                 res.append(p_copy[:])
 
         return res
+
+# 2. Iteration
+
+# class Solution(object):
+#     def permute(self, nums):
+
+#         perm = [[]]
+
+#         for i in nums:
+#             new_perm = []
+#             for p in perm:
+#                 for j in range (len(p)+1):
+#                     p_copy = p[:]
+#                     p_copy.insert(j,i)
+#                     new_perm.append(p_copy[:])
+#             perm = new_perm[:]
+#         return perm
+
+
 
 obj = Solution()
 
