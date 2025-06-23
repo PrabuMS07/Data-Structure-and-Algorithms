58aba2be8e9bff36c574df8e9933523ced130864
prabums07 <msprabu12345@gmail.com>
2025-06-23 20:13:12 +0530

    136. Single Number

---
+++ Bit Manipulation/136. Single Number.py
@@ -0,0 +1,23 @@
+"""Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [2,2,1]
+
+Output: 1
+"""
+
+class Solution(object):
+    def singleNumber(self, nums):
+        res = 0
+        for i in nums:
+            res ^= i
+        return res
+
+obj = Solution()
+
+print(obj.singleNumber(nums = [2,2,1]))
