3f1a71e5b24f303f44ac4240fea4fe796364df79
prabums07 <msprabu12345@gmail.com>
2025-08-08 10:32:00 +0530

    Depth First Search in recursive

--- Tree/94. Binary Tree Inorder Traversal.py
+++ Tree/94. Binary Tree Inorder Traversal.py
@@ -29,16 +29,29 @@ Output: [4,2,6,5,7,1,3,9,8]
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
 
-        stack = []
-        res = []
-        cur = root
-        while cur or stack:
-            while cur:
-                stack.append(cur)
-                cur = cur.left
-            cur = stack.pop()
-            res.append(cur.val)
-            cur = cur.right
+        # stack = []
+        # res = []
+        # cur = root
+        # while cur or stack:
+        #     while cur:
+        #         stack.append(cur)
+        #         cur = cur.left
+        #     cur = stack.pop()
+        #     res.append(cur.val)
+        #     cur = cur.right
         
+        # return res
+
+
+        res = []
+        def inorder (root):
+            if not root:
+                return 
+            
+            inorder(root.left)
+            res.append(root.val)
+            inorder(root.right)
+        inorder(root)
         return res
-    
+
+        
